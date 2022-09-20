2cfa1b7a2bf5661fd193fbbd647bbb33076dd061
0xJoeMama <0xjoemama@gmail.com>
2022-09-20 21:43:09 +0300

    Minor telescope improvements

--- lua/me/config/telescope.lua
+++ lua/me/config/telescope.lua
@@ -2,6 +2,11 @@ require("me.util").safe_run("telescope", function(telescope)
   telescope.setup {
     defaults = {
       color_devicons = true,
+      scroll_strategy = "limit",
+      prompt_prefix = " ",
+      selection_caret = "🡆 ",
+      multi_icon = "🡆 ",
+      history = false,
     },
     pickers = {
       find_files = {

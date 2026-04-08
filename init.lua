4a947f461bc7f6188754d6db7abf16dea4cfc04e
0xJoeMama <0xjoemama@gmail.com>
2026-04-08 17:08:07 +0300

    feat: rewrite config for simplicity

--- init.lua
+++ init.lua
@@ -1,62 +1,151 @@
--- firstly enable luacache loader
-vim.loader.enable()
+local util = require("me.util")
 
-local config_main = function()
-  -- then try to load util since it's needed everywhere
-  local ok, util = pcall(require, "me.util")
+util.apply(vim.opt) {
+  -- ***only*** use UTF-8 for character encoding
+  encoding = "utf-8",
+  -- disable vi compat
+  compatible = false,
+  -- delay counting of keys or whatever its called
+  timeoutlen = 800,
+  -- hide closed buffers
+  hidden = true,
+  -- line numbers
+  number = true,
+  relativenumber = true,
+  -- 2 wide tabs made of spaces
+  tabstop = 2,
+  shiftwidth = 2,
+  expandtab = true,
+  smarttab = true,
+  autoindent = true,
+  smartindent = true,
+  shiftround = true, -- round to shiftwidth
+  -- mouse off for the love of god
+  mouse = "",
+  -- background
+  background = "dark",
+  -- disable all backup functions except for undo files
+  -- set the correct directory for those
+  swapfile = false,
+  backup = false,
+  writebackup = false,
+  undodir = os.getenv("HOME") .. "/.cache/nvim/undoes",
+  undofile = true,
+  -- always add a signcolumn
+  signcolumn = "yes",
+  -- faster will make ui update quicker at the cost of performance
+  updatetime = 200,
+  -- colours
+  termguicolors = true,
+  -- search options
+  hlsearch = false, -- disables highlight
+  ignorecase = true, -- together with smartcase makes searches ignore casing except when a capital is present
+  smartcase = true, -- ^^
+  incsearch = true, -- show search results *while* the pattern is being typed out
+  cmdheight = 1, -- make the command bar smoll
+  conceallevel = 0,
+  pumheight = 10,
+  -- disable the current mode indicator, as the statusline handles that for us
+  showmode = false,
+  -- scroll down 8 items earlier
+  scrolloff = 8,
+  -- scroll to the side 8 items earlier
+  sidescrolloff = 8,
+  -- do not wrap lines for the love of god
+  wrap = false,
+  -- always display the statusline
+  laststatus = 3,
+  -- neovide font
+  guifont = "JetBrainsMono Nerd Font:h10",
+  -- highlight current line
+  cursorline = true,
+  clipboard = function(clip)
+    -- makes vim immediate register correspond to the system clipboard and vise-versa
+    clip:append("unnamedplus")
+  end,
+  whichwrap = function(wrap)
+    -- consider these jumpable when on different lines
+    wrap:append("<,>,[,],h,l")
+  end,
+  iskeyword = function(keywords)
+    -- make '-' separated words count as a singular word
+    keywords:append("-")
+  end,
+  splitright = true,
+  splitbelow = true,
+  confirm = true,
+  pumblend = 10,
+  winblend = 10,
+  fillchars = function(fillchars)
+    fillchars:append("eob: ")
+  end,
+  formatoptions = function(formatoptions) -- this does not work ?! :concern:
+    formatoptions:remove("c")
+    formatoptions:remove("r")
+    formatoptions:remove("o")
+  end,
+  foldmethod = "marker", -- markers are cool, give them a try with {\{{ and 'za'
+  guicursor = "",
+}
 
-  -- exit the config early, since the util module *is* required for any of this to work
-  if not ok then
-    return
-  end
+util.apply(vim.g) {
+  mapleader = " ",
+  maplocalleader = " ",
+  loaded_netrw = 1,
+  loaded_netrwPlugin = 1,
+  loaded_node_provider = 0,
+  loaded_python3_provider = 0,
+  loaded_ruby_provider = 0,
+  loaded_perl_provider = 0,
+}
 
-  -- load the config
-  local err, issues = util.load_modules {
-    me = {
-      "options",
-      "keymaps",
-      "autocmds",
-      "plugins",
-      config = {
-        "telescope",
-        "nvim-treesitter",
-        "which-key",
-        "notify",
-        "nvim-tree",
-        "nvim-surround",
-        "nvim-autopairs",
-        "rainbow-delimiters",
-        "lualine",
-        "gitsigns",
-        "comment",
-        "alpha",
-        "blink",
-        "presence"
+
+vim.pack.add {
+  "https://github.com/ellisonleao/gruvbox.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/neovim-treesitter/nvim-treesitter",
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/mason-org/mason.nvim",
+  "https://github.com/mason-org/mason-lspconfig.nvim",
+}
+
+vim.cmd.colorscheme("gruvbox")
+
+local ts_lingas = {
+  "c", "lua", "cpp", "rust"
+}
+
+require("nvim-treesitter").install(ts_lingas)
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = ts_lingas,
+  callback = function()
+    vim.treesitter.start()                                    -- highlighting
+    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'     -- folds
+    -- vim.wo.foldmethod = 'expr'
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
+  end,
+})
+
+vim.cmd.syntax("on")
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+vim.lsp.config("lua_ls", {
+  ---@type lspconfig.settings.lua_ls
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
       },
-      "lsp",
-      "colors",
-      ui = {
-        "lsp",
+      workspace = {
+        preloadFileSize = 10000,
+        library = {
+          vim.env.VIMRUNTIME,
+        }
       },
     },
-  }
+  },
+})
 
-  -- create an error message if there was an issue with some module
-  if err then
-    local issue_string = ""
-    for _, issue in ipairs(issues) do
-      issue_string = issue_string .. "'" .. issue .. "', "
-    end
-
-    vim.notify("Modules " .. issue_string .. "were invalid!", vim.log.levels.WARN, {
-      title = "Woops, there was issue!",
-    })
-  end
-end
-
--- run it, in protected mode to catch any and all errors
-local ok, _ = pcall(config_main)
-if not ok then
-  print("Something went terribly wrong! Exiting the config!")
-  vim.cmd([[qa!]])
-end
